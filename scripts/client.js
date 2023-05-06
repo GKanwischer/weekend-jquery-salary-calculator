aceb15ad64fd64d5c68136d6cc0b51234260930d
Garret Kanwischer <kanwischer.garret@gmail.com>
2023-05-06 13:39:17 -0500

    Updated deleteRow function

--- scripts/client.js
+++ scripts/client.js
@@ -1,22 +1,24 @@
 let monthSal = 0;
+let totalSal = 0;
+let annSal = 0;
 const monthLimit = 20000;
 
 // sets up jquery
 $(function(){
     // click event for submitButton
-    $('#submitButton').on( 'click', appendTable );
-    $('#deleteButton').on( 'click', deleteRow );
+    $('#submitButton').on( 'click', updateDom );
+    $('table').on('click',".deleteButton", deleteRow );
 });
 
 // adds items to the table upon button click
-function appendTable(){
-    // set variable equal to input fields
+function updateDom(){
+    // set variables equal to the input fields
     const tableBody = $('tbody');
     const firstName = $('#firstIn');
     const lastName = $('#lastIn');
     const idNum = $('#idNumIn');
     const title = $('#titleIn');
-    const annSal = $('#annSalIn');
+    annSal = $('#annSalIn');
 
     // stops and alerts the user if the form is incomplete
     if(
@@ -37,24 +39,51 @@ function appendTable(){
         <td>${lastName.val()}</td>
         <td>${idNum.val()}</td>
         <td>${title.val()}</td>
-        <td>${annSal.val()}</td>
-        <td>
-            <button class="deleteBtm">Delete</button>
-        </td>
+        <td data-salary="${annSal.val()}">${annSal.val()}</td>
+        <td><button class="deleteButton">Delete</button></td>
     </tr>`;
+
+    // add the inputted salary to the running total
+    totalSal += (Number(annSal.val()));
+
+    // calculate the new monthly salary
+    monthSal = (totalSal / 12);
+
     // add the inputted content into the table
     tableBody.append(content);
-    // set the value the input fields to an empty string after 'sumbitting'
+
+    // update the total monthly salary on the DOM
+    $('#monthlyIncome').text(monthSal);
+
+    // set the value in the input fields to an empty string after 'sumbitting'
     firstName.val('');
     lastName.val('');
     idNum.val('');
     title.val('');
     annSal.val('');
-}
+} // end updateDom
+
 // function to remove row after a delete button is clicked
 function deleteRow(event) {
     // set a variable to hold the target
     const deleteBtn = $(event.target);
-    // remove the closest row to the target
-    deleteBtn.closest('tr').remove();
-}
+
+    // set variable to hold which row is targeted by the delete button
+    const deletedRow = deleteBtn.closest('tr');
+
+    // set variable to hold the annual salary value of the targeted row
+    const deletedSalary = Number(deletedRow.find('td:eq(4)').data('salary'));
+
+    // remove targeted row's salary ammount from the running total
+    totalSal -= deletedSalary;
+
+    // calculate new monthly salary
+    monthSal = (totalSal / 12);
+
+    // remove the targeted row
+    deletedRow.remove();
+
+    // update the total monthly salary on the DOM
+    $('#monthlyIncome').text(monthSal);
+
+} // end deleteRow
